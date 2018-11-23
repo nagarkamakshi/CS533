21cb762928162d91d8fdd8a82451cbb3c7ca5716
mukhsia <mukhsia@pdx.edu>
2018-11-22 16:44:30 -0800

    Update my_driver.c

--- my_driver.c
+++ my_driver.c
@@ -5,9 +5,9 @@
 #include <linux/genhd.h>
 #include <linux/fs.h>
 #include <linux/blkdev.h>
+#include <linux/kernel.h>	/* printk() */
 
-
-#define MY_BLOCK_MAJOR           240
+#define MY_BLOCK_MAJOR           240	//240-254 is for local/ experimental use
 #define MY_BLKDEV_NAME          "mybdev"
 #define NR_SECTORS                   1024
 #define MY_BLOCK_MINORS       1
@@ -17,6 +17,8 @@ static struct my_block_dev {
     //...
     // TODO data array or a data structure where the data is stored ?
     // Maybe need to study CH.8 of ldd3 book
+	unsigned long size;             /*Device size*/
+	u8 *data;                       /*Data array */
     spinlock_t lock;                /* For mutual exclusion */
     struct request_queue *queue;    /* The device request queue */
     struct gendisk *gd;             /* The gendisk structure */
@@ -39,10 +41,12 @@ static int my_block_release(struct gendisk *gd, fmode_t mode)
     return 0;
 }
 
+//TODO myblock_ioctl?
+
 struct block_device_operations my_block_ops = {
     .owner = THIS_MODULE,
     .open = my_block_open,
-    .release = my_block_release,
+    .release = my_block_release
 	//.rw_page = my_block_rw_page
 	//.ioctl = my_block_ioctl,
 	//.direct_access = my_block_direct_access
@@ -58,10 +62,20 @@ static int create_block_device(struct my_block_dev *dev)
     //Check whether it will have repercussions with the perf monitor and file r/w load distribution
     //(Maybe sequential access will make file split/ partitioning load distribution pointless as device I/O or the r/w request is processed 1-by-1)
 	
-    //Register block device, optional, tradition
+	//From sample Allocate vmem for data array
+    memset (dev, 0, sizeof (struct sbull_dev));
+    dev->size = NR_SECTORS * KERNEL_SECTOR_SIZE;
+    dev->data = vmalloc(dev->size);
+    if (dev->data == NULL) {
+        printk (KERN_NOTICE "CS533: vmalloc failure.\n");
+        return;
+    }
+	
+    //Register block device, optional since we statically define major number 240, tradition
     if(register_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME) <= 0)
     {
         printk(KERN_WARNING "CS533: register_blkdev failure\n");
+        vfree(dev->data);
         return -EBUSY;
     }
 	
@@ -70,6 +84,7 @@ static int create_block_device(struct my_block_dev *dev)
     if (!dev->gd) {
         printk (KERN_NOTICE "CS533: alloc_disk failure\n");
         unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME)
+        vfree(dev->data);
         return -ENOMEM;
     }
 
@@ -85,7 +100,9 @@ static int create_block_device(struct my_block_dev *dev)
 	
 	/* Initialize the I/O queue */
     spin_lock_init(&dev->lock);
-    dev->queue = blk_init_queue(my_block_request, &dev->lock);
+    dev->queue = blk_init_queue(my_block_request, &dev->lock);	//If we will use Random Access/ no request queue
+	                                                            //use blk_alloc_queue(GFP_KERNEL) instead
+																//and implement a make request function
     if (dev->queue == NULL)
         goto out_err;
     blk_queue_logical_block_size(dev->queue, KERNEL_SECTOR_SIZE);
@@ -99,9 +116,11 @@ static int create_block_device(struct my_block_dev *dev)
     del_gendisk(dev->gd);	
     put_disk(dev->gd);		
     unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
+    vfree(dev->data);vfree(dev->data);
     return -ENOMEM;
 }
 
+//The request function depends on
 static void my_block_request(struct request_queue *q)
 {
     struct request *rq;
@@ -119,7 +138,7 @@ static void my_block_request(struct request_queue *q)
         }
 
         /* do work */
-        //... Maybe call the r/w function? or handled by bio?
+        //... Maybe call the r/w function? or handled by bio? or simple/ full transfer request?
 
         __blk_end_request_all(rq, 0);
     }
@@ -148,6 +167,9 @@ static void delete_block_device(struct my_block_dev *dev)
         del_gendisk(dev->gd);
         put_disk(dev->gd);
     }
+	if(dev->data) {
+        vfree(dev->data);
+	}
     //...
 }
 
